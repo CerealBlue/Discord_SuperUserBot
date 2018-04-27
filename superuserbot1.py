5e2fe14de55a30745b31fca73d2e1ed80ef554e6
BlueCereal <35899273+CerealBlue@users.noreply.github.com>
2018-04-27 21:07:33 +0530

    Fixed Bugs from previous commit

--- superuserbot1.py
+++ superuserbot1.py
@@ -2,38 +2,37 @@
 
 from discord.ext.commands import Bot
 from discord import Game
-from datetime import datetime
+import datetime
 import sys
 import asyncio
 import random
 
 #sys.path.append("/home/CerealBlue/Desktop/Discord/SuperUserBot/MemeTaker_Bot/")
 
-from mt_b import mt_b_start as memesteal
+#from mt_b import mt_b_start as memesteal
 
-#TOKEN =
+TOKEN = "NDM0MDI3NzE2Nzk1NDMyOTYx.DcTOsQ.yxwiSAePR3u_wlXgh_pt9SuW6_o"
 
 BOT_PREFIX = ("?", "!", "++")
 
 client = Bot(command_prefix=BOT_PREFIX)
 
 @client.command(name='greetings',
-                descripition="This command makes SuperUserBot greet you.",
-                brief="Bot Greets"
-		        aliases=['hey','hi','hello', 'sup', 'wassup'],
-		        pass_context = True)
+                description="This command makes SuperUserBot greet you.",
+                brief="Bot Greets",
+		aliases=['hey','hi','hello', 'sup', 'wassup'],
+		pass_context = True)
 async def greet(context):
     greeters = [
         "Wassup,",
         "Salutations",
         "Hello from the Cloud!",
-        "My Creator (The great BlueCereal) told me to greet you! Greetings,",
-        "Hello! I stay in a Raspberry Pi! It's doesn't taste like a Pie though :( ..."
-    ]
-	await client.say(random.choice(greeters) + " " + context.message.author.mention + ".")
+        "Hello! I stay in a Raspberry Pi! It's doesn't taste like a Pie though :( ...",
+        "My Creator (The great BlueCereal) has taught me to greet people (I'm anti-social, btw). Here goes... ~~Gretings~~ ~~Greatthings~~ \n\nGreetings,"]
+    await client.say(random.choice(greeters) + " " + context.message.author.mention + ".")
 
 @client.command(name='time',
-                descripition=
+                description=
                 "This command displays the date and time in:"
                 "\n\t>YYYY-MM-DD HH:MM:SS:MS"
                 "\n\t>Current Year"
@@ -42,20 +41,20 @@ async def greet(context):
                 "\n\t>WeekdayNumber"
                 "\n\t>Day of the Year"
                 "\n\t>Day of the Month"
-                "\n\t>Day of the Week""
-                "\n\nAlso, why are you this lazy to not look at your phone's time?"
-                brief="Time Displayer"
+                "\n\t>Day of the Week"
+                "\n\nAlso, why are you this lazy to not look at your phone's time?",
+                brief="Time Displayer",
                 aliases=['date', 'day'])
 async def timeDisplay():
 	await client.say("Time:\n"
                     "```"+str(datetime.datetime.now())+"```\n"
-                    "Current year: `" +     str(datetime.date.today().strftime("%Y")) + "`\n"
-                    "Current Month: `" +    str(datetime.date.today().strftime("%B")) + "`\n"
-                    "WeekNumber `" +        str(datetime.date.today().strftime("%W")) + "`\n"
-                    "WeekdayNumber: `" +    str(datetime.date.today().strftime("%w")) + "`\n"
-                    "Day of the Year: `" +  str(datetime.date.today().strftime("%j")) + "`\n"
-                    "Day of the Month: `" + str(datetime.date.today().strftime("%d")) + "`\n"
-                    "Day of the Week: `" +  str(datetime.date.today().strftime("%A")) + "`\n"
+                    "Current year: \t`" +     str(datetime.date.today().strftime("%Y")) + "`\n"
+                    "Current Month: \t`" +    str(datetime.date.today().strftime("%B")) + "`\n"
+                    "WeekNumber \t`" +        str(datetime.date.today().strftime("%W")) + "`\n"
+                    "WeekdayNumber: \t`" +    str(datetime.date.today().strftime("%w")) + "`\n"
+                    "Day of the Year: \t`" +  str(datetime.date.today().strftime("%j")) + "`\n"
+                    "Day of the Month: \t`" + str(datetime.date.today().strftime("%d")) + "`\n"
+                    "Day of the Week: \t`" +  str(datetime.date.today().strftime("%A")) + "`\n"
                     )
 
 @client.async_event
@@ -63,7 +62,7 @@ async def on_message(message):
 	chan = message.channel
 	if message.content.startswith('Bot'):
 		await client.send_message(message.channel, "hi")
-	if message.content.startswith('SendMeme'):
+	"""if message.content.startswith('SendMeme'):
 		await client.send_message(message.channel, "Wait for a couple of secondz. Pls.\n\nFetching only the best may-mays from:\nReddit!")
 		memesteal()
 
@@ -73,7 +72,7 @@ async def on_message(message):
 
 		with open('MemeTaker_Bot/Memes/Meme'+str(number-1)+'.jpeg', 'rb') as f:
 			await client.send_file(message.channel, f)
-		await client.send_message(message.channel, "Okay?")
+		await client.send_message(message.channel, "Okay?")"""
 	await client.process_commands(message)
 
 @client.event
